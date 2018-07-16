4f0470736993f0a5da3ab150e61c7af44121bc33
Allan Liebold <allan.liebold@gmail.com>
2018-07-16 06:16:06 -0700

    Update bst.js

--- bst.js
+++ bst.js
@@ -6,9 +6,18 @@ function BST(value) {
 
 BST.prototype.insert = function(value) {
   if (value <= this.value) {
-    
+    if (!this.left) {
+      this.left = new BST(value);
+    }
+    else {
+      this.left.insert(value);
+    }
   } else if (value > this.value) {
-    
+    if (!this.right) {
+      this.right = new BST(value);  
+    } else {
+      this.right.insert(value);
+    }
   }
 };
 
