d557dabd11e372fc5adeef1435af0cf8696d5705
Allan Liebold <allan.liebold@gmail.com>
2018-07-17 06:07:17 -0700

    Update bst.js

--- bst.js
+++ bst.js
@@ -25,9 +25,17 @@ BST.prototype.contains = function(value) {
   if (value === this.value) {
     return true; 
   } else if (value < this.value) {
-     
+     if(!this.left) {
+      return false; 
+     } else {
+      return this.left.contains(value); 
+     }
   } else if (value > this.value) {
-    
+    if(!this.right) {
+      return false; 
+    } else {
+      return this.right.contains(value); 
+    }
   }
 };
 
